eca51a25e1991be6f10eb9b550dd2e9f9662f660
Hadi Afsar <hadiafsar12.2000@gmail.com>
2026-06-30 16:24:45 +0530

    removed target directory

--- target/debug/snake-cli.d
+++ target/debug/snake-cli.d
@@ -1,1 +1,1 @@
-/home/hadia/projects/snake-cli/target/debug/snake-cli: /home/hadia/projects/snake-cli/src/main.rs
+/home/hadia/projects/snake-cli/target/debug/snake-cli: /home/hadia/projects/snake-cli/target/package/snake-cli-0.1.0/src/main.rs
